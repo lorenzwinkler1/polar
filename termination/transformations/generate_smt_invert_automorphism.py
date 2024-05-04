11ba4f99e3731b75895c4808b5b3d080a10e6548
Lorenz Winkler <lorenz.winkler@tuwien.ac.at>
2024-05-04 19:42:28 +0200

    fix some stuff, add u' requirement

--- termination/transformations/generate_smt_invert_automorphism.py
+++ termination/transformations/generate_smt_invert_automorphism.py
@@ -1,10 +1,11 @@
 # Validated with example 5.2 form paper https://doi.org/10.1007/s10703-023-00440-z
 
-from sympy import Eq, Mul, Poly, Pow, sympify, Symbol, smtlib_code
+from sympy import Eq, Mul, Poly, Pow, expand, sympify, Symbol, smtlib_code
 
 
 NUM_VARS = 2
 MAX_DEGREE = 2
+DO_LABEL = True
 
 
 def get_monom_tuples(num_vars, max_degree):
@@ -32,12 +33,17 @@ monomials = [get_monomial(vars, coeff) for coeff in monomial_tuples]
 
 def n(monom):
     if not monom.is_symbol:
+        monom = expand(monom)
         symbols = monom.free_symbols
 
-        substitutes = {sym: n(sym) for sym in symbols}
-        return monom.subs(substitutes)
+        substitutes = {Symbol(str(var)+"_TMP", real=True): expand(n(sym)) for sym in symbols}
+
+        monom = monom.subs({var: Symbol(str(var)+"_TMP", real=True) for var in vars})
+        return expand(monom.subs(substitutes))
     
     # refers tu x_i
+    if not str(monom).startswith("x_"):
+        return monom
     i = str(monom).split("_")[1]
 
     term = sympify(0)
@@ -77,16 +83,11 @@ for i,var in enumerate(vars):
 
 
 for i, var in enumerate(vars):
-    print("=================")
     eq = n(var)
-    # print(eq)
     # substitute in inverse dict
     eq = eq.subs({var: Symbol(str(var)+"_TMP", real=True) for var in vars})
 
     eq = eq.subs(inverse_dict)
-    # print(inverse_dict)
-    # print()
-    # print(eq)
     poly = Poly(eq, *vars)
     coeffs = poly.coeffs()
     monoms = poly.monoms()
@@ -96,15 +97,72 @@ for i, var in enumerate(vars):
         exprs.append(Eq(coeffs[k], Symbol(f"p_l_{i}_{'_'.join(map(str, monoms[k]))}", real = True)))
         exprs.append(Eq(Symbol(f"p_l_{i}_{'_'.join(map(str, monoms[k]))}", real = True), int(should_be_one)))
 
+# Now the u' requirement
+
+# Later u is the input!
+
+us = [expand(((-vars[1]**2+vars[0])**2 + vars[1])**2 - 2*vars[1]**2+2*vars[0]),\
+      expand((-vars[1]**2+vars[0])**2 + vars[1])]
+
+assert len(us) == NUM_VARS
+
+n_s = [n(var).subs({var: Symbol(str(var)+"_TMP", real=True) for var in vars})
+        for var in vars]
+
+# u ( n^{-1} (x))
+
+u_dashs = {
+   Symbol(str(var)+"_TMP", real=True): expand(u) for i, u in enumerate(us)
+}
+
+ns = [expand(n.subs(u_dashs)).subs({var: Symbol(str(var)+"_TMP", real=True) for var in vars}) for n in n_s]
+
+pass
+
+
+n_prime_s = {}
+
+for i,var in enumerate(vars):
+    # p_{r,i}, where x_i is the var
+    eq = sympify(0)
+    for j in range(len(monomials)):
+        mono = monomials[j]
+        monomial_tuple = monomial_tuples[j]
+        eq += Symbol(f"a_{i}__{'_'.join(map(str, monomial_tuple))}")*mono
+    n_prime_s[Symbol(str(var)+"_TMP", real=True)] = eq
+
+n_s = [n.subs(n_prime_s) for n in n_s]
+
+n_s = [expand(n) for n in n_s]
+
+# now substitute inverse
+
+coeffs_to_assert_zero = []
+# Now do comparison of coefficients again
+for i, var in enumerate(vars):
+    u_dash = n_s[i]
+
+    poly = Poly(u_dash, *vars)
+    coeffs = poly.coeffs()
+    monoms = poly.monoms()
+
+    for k in range(len(coeffs)):
+        sym = Symbol(f"c_{i}_{'_'.join(map(str, monoms[k]))}", real = True)
+        exprs.append(Eq(coeffs[k], sym))
+
+        if monoms[k][i] > 0 or (monoms[k][i]==1 and sum(monoms[k])>1):
+            exprs.append(Eq(sym, 0))
 
 
 smt_str = smtlib_code(exprs)
 smt_str = str(smt_str)
-smt_str = smt_str.replace("(=", "(! (=")
 
 i=0
-while smt_str.find("))\n")>0:
-    smt_str = smt_str.replace(f"))\n", f") :named a{i}) )\n" ,1)
-    i+=1
+if DO_LABEL:
+    smt_str = smt_str.replace("(=", "(! (=")
+
+    while smt_str.find("))\n")>0:
+        smt_str = smt_str.replace(f"))\n", f") :named a{i}) )\n" ,1)
+        i+=1
 
 print(smt_str)
